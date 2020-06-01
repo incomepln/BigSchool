1f1f316523db3825c2320082a33df30dcb78235f
Loc <phuocloc123vn1999@gmail.com>
2020-06-01 16:26:12 +0700

    Update Today

---
+++ LAB345/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using LAB345.Models;
+using LAB345.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB345.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
