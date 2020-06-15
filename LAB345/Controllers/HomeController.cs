90f2ad6f2684bf08790d018d4854ff8e3e6b8e0e
Loc <phuocloc123vn1999@gmail.com>
2020-06-15 15:40:23 +0700

    Loc pro

--- LAB345/Controllers/HomeController.cs
+++ LAB345/Controllers/HomeController.cs
@@ -1,23 +1,34 @@
-﻿using System;
+﻿using LAB345.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace LAB345.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
-           
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
+      
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-           
+
             return View();
         }
 
@@ -26,6 +37,6 @@ namespace LAB345.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
+        }  
     }
 }
